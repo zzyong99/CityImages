5f07b8649b40220c524292233391c7f24b43050f
zzyong99 <115106028+zzyong99@users.noreply.github.com>
2023-09-16 21:42:01 -0700

    CreateCity Images App

--- CityViews/ViewController.swift
+++ CityViews/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var IVView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        IVView.image = UIImage(named: "SeaImage")
     }
 
-
+    @IBAction func ClickSea(_ sender: Any) {
+        IVView.image = UIImage(named: "SeaImage")
+    }
+    @IBAction func clickLA(_ sender: Any) {
+        IVView.image = UIImage(named: "LAImage")
+    }
+    @IBAction func clickAus(_ sender: Any) {
+        IVView.image = UIImage(named: "AusImage")
+    }
+    @IBAction func clickNY(_ sender: Any) {
+        IVView.image = UIImage(named: "NYImage")
+    }
+    @IBAction func clickVeg(_ sender: Any) {
+        IVView.image = UIImage(named: "VegImage")
+    }
 }
 
